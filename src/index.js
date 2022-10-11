65bd368e2fc53fc1575d0b1be0eba4e28bf6fd37
allanadiogo <ra54761765860@acaonsfatima.org.br>
2022-10-11 16:48:23 -0300

    s

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import Acai from './pages/acai'
 import Gramas from './pages/gramas'
 import Signo from './pages/signo'
 import Home from './pages/home'
+import Salario from './pages/salario'
+import Temperatura from './pages/temperatura'
+import Paradas from './pages/paradas'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,6 +24,11 @@ root.render(
     <Route   path='/gramas' element={<Gramas/>} />
     <Route   path='/signo' element={<Signo/>} />
     <Route   path='/' element={<Home/>} />
+    <Route   path='/salario' element={<Salario/>} />
+    <Route   path='/temperatura' element={<Temperatura/>} />
+    <Route   path='/paradas' element={<Paradas/>} />
+
+
 
   </Routes>
   </BrowserRouter>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,11 +3,14 @@ import './index.scss'
 export default function Index(){
     return(
         <main className='todo'>
+            <div className='a'>
         <h1>Prova</h1>
         <a href="/acai">Açai</a>
         <a href="/signo">Signo</a>
         <a href="/gramas">Gramas</a>
+        <a href="/salario">Salario</a>
 
+</div>
 </main>
     )
 }
