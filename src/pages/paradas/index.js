f7e50cbb2344d0288a64cb0dd6ddf62f1964d221
sucoDfruta <noteebiel123@gmail.com>
2022-10-11 18:11:52 -0300

    qualuqer coisa

--- src/pages/paradas/index.js
+++ src/pages/paradas/index.js
@@ -1,5 +1,81 @@
+import { useState, useRef, useEffect} from 'react';
+
 export default function Index(){
+
+    const [Capac, setCapac] = useState();
+    const [Con, setConsumo] = useState();
+    const [Dist, setDistancia] = useState();
+
+
+    const [resposta, setResposta] = useState('');
+    const [carregando, setCarregando] = useState('');
+    const [erro, setErro] = useState('');
+    const ref = useRef();
+    
+
+    
+    function GasolinaParadas(){
+        try{
+            let x = Dist / Capac;
+            let y = x / Con
+
+            setResposta(Math.ceil(y))
+    }
+    catch (err) {
+        ref.current.complete()
+        setCarregando(false)
+        if (err.message.status === 401) {
+            setErro(err.message.data.Erro)
+        }
+    }
+}
+
+useEffect(() => {
+    GasolinaParadas()
+}, [Capac, Con, Dist])
+
+    
+    
+
+
+
+    
+
     return(
-        <h1>sdpow</h1>
+        <main>
+            <h1>Verificar quantas paradas precisam ser feitas</h1>
+
+            <div>
+
+            
+        
+        <input type="text" value={Con} placeholder="consumo" onChange={e =>  setConsumo(e.target.value)} ></input>
+        
+        </div>
+
+        <div>
+        <input type='text' value={Capac}  placeholder="capacidade" onChange={e =>  setCapac(e.target.value)}></input>
+        </div>
+
+        <div>
+        <input type='text' value={Dist}  placeholder="distancia" onChange={e =>  setDistancia(e.target.value)}></input>
+        </div>
+
+        <div>
+        <button className='' onClick={GasolinaParadas} disabled={carregando}>
+            Verificar
+        </button>
+        </div>
+
+        <div>
+            Quantas paradas eu preciso fazer?  <span> {resposta} </span>
+        </div>
+
+        <div>
+            {erro}
+        </div>
+
+        
+        </main>
     )
 }
