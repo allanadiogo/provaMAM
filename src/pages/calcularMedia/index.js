f4c9255165da93acd0c15019f9006c2202dc441d
allanadiogo <ra54761765860@acaonsfatima.org.br>
2022-10-25 17:18:21 -0300

    mds

--- src/pages/calcularMedia/index.js
+++ src/pages/calcularMedia/index.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect} from 'react';
+import { useState, useRef} from 'react';
 
 export default function Index(){
 
@@ -18,8 +18,6 @@ export default function Index(){
   
  
 
-
-  
 
 
     return(
