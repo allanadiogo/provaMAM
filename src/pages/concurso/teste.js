28c277678fdc0b4fd107b537a7111fb81f809c85
sucoDfruta <noteebiel123@gmail.com>
2022-11-08 17:45:10 -0300

    o theusin mudou

---
+++ src/pages/concurso/teste.js
@@ -0,0 +1,25 @@
+const readLine = require('prompt-sync')();
+function Sorteio( quantidade, tentativas){
+    let numero = parseInt(Math.random() * 1000)
+    console.log('insira o valor')
+    for(let i = 1; i  <= quantidade ; i++){
+        let chute = readLine();
+        if(chute == numero){
+            console.log('acertou')
+            break
+        }
+        else if(chute < numero){
+            console.log('Maior !! ' )
+        }
+        else{
+            console.log('Menor !! ' )
+        }
+
+    }
+    console.log(numero)
+
+}
+function Main(){
+    Sorteio(10)
+}
+Main();
